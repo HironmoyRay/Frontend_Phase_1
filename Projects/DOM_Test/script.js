081cd8a0fd575929a45ef9de99cc8251474c0c0f
hironmoy-ray <hpray35@gmail.com>
2024-11-17 22:53:27 +0600

    For Quiz

---
+++ Projects/DOM_Test/script.js
@@ -0,0 +1,33 @@
+// document.addEventListener("click", function () {
+//   console.log("Document clicked");
+// });
+
+// document.getElementById("myButton").addEventListener("click", function (event) {
+//   console.log("Button clicked");
+//   event.stopPropagation();
+// });
+
+// document.addEventListener("keydown", function (event) {
+//   console.log(event.key);
+// });
+
+// const all = document.querySelectorAll(".class");
+// console.log(all);
+
+// document.getElementById("myDiv").addEventListener("click", function () {
+//   console.log(this);
+// });
+// document.getElementById("parent").addEventListener("click", function (event) {
+//   console.log("currentTarget:", event.currentTarget); // Always 'parent'
+//   console.log("target:", event.target); // Could be 'parent' or 'child'
+// });
+
+// document.getElementById("myButton").addEventListener("click", function(event) {
+
+//     event.target.style.backgroundColor = "blue";
+
+//   });
+
+// document.getElementById("myDiv").innerHTML = "Hello World";
+// document.getElementById("myDiv").innerText = "Hello World";
+document.getElementById("myDiv").value = "Hello World";
