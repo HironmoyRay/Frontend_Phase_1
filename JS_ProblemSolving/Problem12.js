b0fa40352dd7bca004811959c3b895c0635f0a61
hironmoy-ray <hpray35@gmail.com>
2024-12-04 23:15:11 +0600

    Problem12

---
+++ JS_ProblemSolving/Problem12.js
@@ -0,0 +1,5 @@
+// Problem 12:
+// Write a function called "getMaxProduct" that takes an array of integers as input and returns the 
+// maximum product that can be obtained by multiplying any two distinct elements from the array.
+// Sample Input: [2, 3, 5, 6, 7]
+// Sample Output: 42 (obtained by multiplying 6 and 7)
